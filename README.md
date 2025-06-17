f82010da8f3f80e18ff6062933f8d2f0583e4cea
dev-deej <d33j@home>
2025-06-17 17:02:27 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tw-test
+
+a sample vue3 project to try and get tailwind v4 running 
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-red-300">
+    Hey Mel...
+  </h1>
+</template>
+
+<style scoped>
+</style>
